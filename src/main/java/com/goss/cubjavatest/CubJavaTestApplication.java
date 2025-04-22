2af13039fcc223a4b224d8734a5ea3879fc20440
gosswu <gosswu@eftpay.com.tw>
2025-04-23 01:49:42 +0800

    Initial Spring Boot setup

---
+++ src/main/java/com/goss/cubjavatest/CubJavaTestApplication.java
@@ -0,0 +1,13 @@
+package com.goss.cubjavatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubJavaTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CubJavaTestApplication.class, args);
+  }
+
+}
